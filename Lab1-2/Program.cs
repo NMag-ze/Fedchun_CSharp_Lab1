c1dba15b93ccb162ef74ca84db496d13e8f03600
NMag-ze <n.magnitowsky@gmail.com>
2020-04-11 03:09:24 +0300

    Add project

---
+++ Lab1-2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Enter 2 numbers:");
+            Console.Write("a = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(x + " + " + y + " = " + (x + y)+"\n");
+
+            //Задание 2
+            Console.WriteLine("Enter 3 numbers:");
+            Console.Write("a = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            int z1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}+{2}={2}+{1}+{0}\n",x1,y1,z1);
+
+            //Задание 3
+            Console.WriteLine("Enter 3 numbers:");
+            Console.Write("a = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            int z2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(x2+"+"+y2+"+"+z2+"="+(x2+y2+z2)+"\n");
+
+            //Задание 4
+            Console.WriteLine("Enter 2 numbers:");
+            Console.Write("a = ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double y3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:.#}*{1:.#}={2:.#}\n",x3,y3,x3*y3);
+
+
+            //Задание 5
+            Console.WriteLine("Enter 2 numbers:");
+            Console.Write("a = ");
+            double x4 = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double y4 = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:.###}/{1:.###}={2:.###}\n", x4, y4, x4 / y4);
+
+            //Задание 6
+            Console.WriteLine("Enter 3 numbers:");
+            Console.Write("a = ");
+            double x5 = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double y5 = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            double z5 = double.Parse(Console.ReadLine());
+            Console.WriteLine("({0:.##}+{1:.##})+{2:.##}={0:.##}+({1:.##}+{2:.##})\n", x5, y5, z5);
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        enum gradus : int
+        {
+            min = 0,
+            krit = 72,
+            max = 100,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            const int i = 10;	
+            Console.WriteLine(i);
+            Console.WriteLine("минимальная температура=" + (int)gradus.min);
+            Console.WriteLine("критическая температура=" + (int)gradus.krit);
+            Console.WriteLine("максимальная температура=" + (int)gradus.max);
+            double x = Math.E;
+            Console.WriteLine("E={0,20}", x);
+            Console.WriteLine("E={0,10}", x);
+            Console.WriteLine("E={0:##.###}", x);
+            Console.WriteLine("E={0:.######}", x);
+
+            Console.WriteLine("C Format:{0,14:C} \t{0:C2}", 12345.678);
+            Console.WriteLine("D Format:{0,14:D} \t{0:D6}", 123);
+            Console.WriteLine("E Format:{0,14:E} \t{0:E8}", 12345.6789);
+            Console.WriteLine("G Format:{0,14:G} \t{0:G10}", 12345.6789);
+            Console.WriteLine("N Format:{0,14:N} \t{0:N4}", 12345.6789);
+            Console.WriteLine("X Format:{0,14:X} ", 1234);
+            Console.WriteLine("P Format:{0,14:P} ", 0.9);
+            
+
+            double z = double.Parse(Console.ReadLine()); //преобразование введенной строки в число
+            Console.WriteLine("z={0:.###}",z);
+
+
+        }
+    }
+}
